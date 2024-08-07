a7eef4786e5a8e6e3ca5c32454e8694f389d37a2
Nico Vandenhove <nico.vandenhove@amexiogroup.com>
2024-08-07 12:30:46 +0200

    Updated card component

--- src/client/components/Card.tsx
+++ src/client/components/Card.tsx
@@ -1,13 +1,15 @@
-export function Card({ product }: { product : {id: string, name: string} }) {
+export function Card({ product }: { product: { id: string; name: string } }) {
   return (
     <div className="flex items-center justify-center max-w-80">
       <div className="w-full p-4">
         <div className="flex flex-col justify-center p-10 bg-white rounded-lg shadow-2xl card">
           <div className="prod-title">
-            <p className="text-2xl font-bold text-gray-900 uppercase">{ product.name }</p>
-            <p className="text-sm text-gray-400 uppercase">
+            <div className="text-2xl font-bold text-gray-900 uppercase">
+              {product.name}
+            </div>
+            <div className="text-sm text-gray-400 uppercase">
               The best shoes in the marketplace
-            </p>
+            </div>
           </div>
           <div className="prod-img pt-4">
             <img
@@ -16,10 +18,10 @@ export function Card({ product }: { product : {id: string, name: string} }) {
             />
           </div>
           <div className="flex flex-col items-center justify-between text-gray-900 md:flex-row mt-8">
-            <p className="text-xl font-bold">65 $</p>
-            <button className="px-6 py-2 uppercase transition duration-200 ease-in border-2 border-gray-900 rounded-full hover:bg-gray-800 hover:text-white focus:outline-none">
+            <div className="text-xl font-bold">65 $</div>
+            <div className="px-6 py-2 uppercase transition duration-200 ease-in border-2 border-gray-900 rounded-full hover:bg-gray-800 hover:text-white focus:outline-none">
               Add to cart
-            </button>
+            </div>
           </div>
         </div>
       </div>
